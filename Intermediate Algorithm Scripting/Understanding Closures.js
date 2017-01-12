b178f2576a033ed9d80f2b9f8b68d29db3275211
dsinecos <dsinecos@gmail.com>
2017-01-12 12:40:00 +0530

    Update Understanding Closures.js

--- Intermediate Algorithm Scripting/Understanding Closures.js
+++ Intermediate Algorithm Scripting/Understanding Closures.js
@@ -17,3 +17,40 @@ console.log(a("Jackson"));
 
 var b = fullName("George");
 console.log(b("Jackson"));
+
+
+/*
+1. Decide the constant to be added or multiplied
+2. Decide whether the final o[peration is to be addition or multiplication
+*/
+
+function test(num) {
+    
+    return {
+        multiplier: function(array) {
+            var newArray = array.map(function(val) {
+                return val * num;
+                });
+            return newArray;
+                
+        },
+      
+        adder: function(array) {
+            var newArray = array.map(function(val) {
+                return val + num;
+            });
+            return newArray;
+        }
+    };
+}
+
+var a = 12;
+
+var b = [1, 2,3, 4, 5, 6, 7, 8, 9];
+
+var c = test(a);
+var d = c.multiplier(b);
+var e = c.adder(b);
+
+console.log(d);
+console.log(e);
